ef314a34e6d4b2bdca3321a971d9d9dfbe6af1db
Nickkontschak <32848650+Nickkontschak@users.noreply.github.com>
2023-04-01 23:40:32 +0200

    Pathlib instead of os.path

--- src/constants.py
+++ src/constants.py
@@ -1,18 +1,20 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 from dataclasses import dataclass
 
-BASE_DIR = os.path.abspath(os.path.dirname(_file_))
-ROOT_DIR = os.path.join(BASE_DIR, "..")
-DATA_PATH = os.path.join(ROOT_DIR, "data")
-MODELS_PATH = os.path.join(ROOT_DIR, "models")
-CACHE_PATH = os.path.join(DATA_PATH, "cache")
+BASE_DIR = Path(__file__).resolve().parent
+ROOT_DIR = BASE_DIR.parent
+ENV_FILE = ROOT_DIR / ".env"
+DATA_PATH = ROOT_DIR / "data"
+MODELS_PATH = ROOT_DIR / "models"
+CACHE_PATH = DATA_PATH / "cache"
 
-
-load_dotenv(ROOT_DIR)
+load_dotenv(ENV_FILE)
 
 @dataclass
 class Config:
     NDL_API_KEY = os.getenv("NASDAQ_DATA_LINK_API_KEY")
+
 
 
